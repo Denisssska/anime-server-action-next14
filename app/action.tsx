5f1f8ccc7b42ec50e8ca3720c5b89959e192b455
Denisssska <yarmoshkoden18m@gmail.com>
2023-12-01 16:39:07 +0300

    first commit

---
+++ app/action.tsx
@@ -0,0 +1,12 @@
+'use server';
+
+import AnimeCard, { AnimeProp } from '@/components/AnimeCard';
+
+export const fetchAnimeShikimori = async(page:number)=>{
+	const res = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`);
+  const data = await res.json();
+	// console.log(data)
+  return data.map((item: AnimeProp, index: number) => (
+		<AnimeCard key={item.id} anime={item} index={index} />
+	))
+}
